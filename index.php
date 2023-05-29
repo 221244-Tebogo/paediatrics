f381add968b0d7a560ef3181b261146b9bda4390
Tebogo Ramolobeng <221244@virtualwindow.co.za>
2023-05-29 03:07:32 +0200

    commit

--- index.php
+++ index.php
@@ -7,63 +7,33 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<!-- Font-->
 	<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;700&display=swap" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;700&display=swap" rel="stylesheet">
 	<!-- Main Style Css -->
-  <link rel="stylesheet" href="./css/style.css">
+	<link rel="stylesheet" href="./css/style.css">
 </head>
-<body class="form-v8">
+<body class="form">
 	<div class="page-content">
-		<div class="form-v8-content">
+		<div class="form-content">
 			<div class="form-left">
-      <img src="https://image.freepik.com/premium-photo/stuffed-bear-animal-presented-as-pediatrician-holding-stethoscope_12506629.jpg" alt="form">
-
+      <img src="https://images.unsplash.com/photo-1611764461465-09162da6465a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1480&q=80" alt="form">
 			</div>
       
 			<div class="form-right">
+      <h2>Paediatrics</h2><br>
+      <h1>Welcome Back</h1><br>
+      <div class="body">
+      Welcome back! Please enter your details.
+      </div>
+				<div class="contact">
 				<div class="tab">
 					<div class="tab-inner">
-						<button class="tablinks" onclick="openCity(event, 'sign-up')" id="defaultOpen">Sign Up</button>
+						<button class="tablinks" onclick="openCity(event, 'sign-in')" id="defaultOpen">Sign In</button>
 					</div>
 					<div class="tab-inner">
-						<button class="tablinks" onclick="openCity(event, 'sign-in')">Sign In</button>
+						<button class="tablinks" onclick="openCity(event, 'sign-up')">Sign Up</button>
 					</div>
 				</div>
-				<form class="form-detail" action="register.php" method="post"> <!-- Replace "register.php" with the PHP file to process the registration form -->
-					<div class="tabcontent" id="sign-up">
-						<div class="form-row">
-							<label class="form-row-inner">
-								<input type="text" name="full_name" id="full_name" class="input-text" required>
-								<span class="label">Username</span>
-		  						<span class="border"></span>
-							</label>
-						</div>
-						<div class="form-row">
-							<label class="form-row-inner">
-								<input type="text" name="your_email" id="your_email" class="input-text" required>
-								<span class="label">E-Mail</span>
-		  						<span class="border"></span>
-							</label>
-						</div>
-						<div class="form-row">
-							<label class="form-row-inner">
-								<input type="password" name="password" id="password" class="input-text" required>
-								<span class="label">Password</span>
-								<span class="border"></span>
-							</label>
-						</div>
-						<div class="form-row">
-							<label class="form-row-inner">
-								<input type="password" name="confirm_password" id="confirm_password" class="input-text" required>
-								<span class="label">Confirm Password</span>
-								<span class="border"></span>
-							</label>
-						</div>
-						<div class="form-row-last">
-							<input type="submit" name="register" class="register" value="Register">
-						</div>
-					</div>
-				</form>
 				<form class="form-detail" action="signin.php" method="post"> <!-- Replace "signin.php" with the PHP file to process the sign-in form -->
 					<div class="tabcontent" id="sign-in">
 						<div class="form-row">
@@ -96,6 +66,41 @@
 						</div>
 						<div class="form-row-last">
 							<input type="submit" name="signin" class="register" value="Sign In">
+						</div>
+					</div>
+				</form>
+				<form class="form-detail" action="register.php" method="post"> <!-- Replace "register.php" with the PHP file to process the registration form -->
+					<div class="tabcontent" id="sign-up">
+						<div class="form-row">
+							<label class="form-row-inner">
+								<input type="text" name="full_name" id="full_name" class="input-text" required>
+								<span class="label">Username</span>
+		  						<span class="border"></span>
+							</label>
+						</div>
+						<div class="form-row">
+							<label class="form-row-inner">
+								<input type="text" name="your_email" id="your_email" class="input-text" required>
+								<span class="label">E-Mail</span>
+		  						<span class="border"></span>
+							</label>
+						</div>
+						<div class="form-row">
+							<label class="form-row-inner">
+								<input type="password" name="password" id="password" class="input-text" required>
+								<span class="label">Password</span>
+								<span class="border"></span>
+							</label>
+						</div>
+						<div class="form-row">
+							<label class="form-row-inner">
+								<input type="password" name="confirm_password" id="confirm_password" class="input-text" required>
+								<span class="label">Confirm Password</span>
+								<span class="border"></span>
+							</label>
+						</div>
+						<div class="form-row-last">
+							<input type="submit" name="register" class="register" value="Register">
 						</div>
 					</div>
 				</form>
