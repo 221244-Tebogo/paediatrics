9683e9b4fbc167c88acd9663791e3e2673aa6bc8
Tebogo Ramolobeng <221244@virtualwindow.co.za>
2023-06-25 22:52:58 +0200

    commit

--- index.php
+++ index.php
@@ -5,168 +5,196 @@ include "db_conn.php";
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<title>Receptionist Form</title>
-	<!-- Mobile -->
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-	<!-- Font-->
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;700&display=swap" rel="stylesheet">
-	<!-- Main Style Css -->
-	<link rel="stylesheet" href="./css/style.css">
+  <meta charset="utf-8">
+  <title>Receptionist Form</title>
+  <!-- Mobile -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <!-- Font-->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;700&display=swap" rel="stylesheet">
+  <link href="css/index.css" rel="stylesheet">
+  <!-- Custom CSS -->
+  <link href="style.css" rel="stylesheet">
+  <style>
+    /* Custom CSS styles */
+    body {
+      margin: 0;
+      padding: 0;
+      font-family: 'Poppins', sans-serif;
+      color: #fff;
+      background-color: #49443c;
+      background-image: url("images/pexels-gustavo-fring-7446987.jpg");
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+    }
+
+    .button {
+      display: inline-block;
+      padding: 10px 20px;
+      background-color: #337ab7;
+      color: #fff;
+      text-decoration: none;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+    }
+
+    .container {
+      position: relative;
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+    }
+
+    .content {
+      flex: 1;
+    }
+
+    .footer {
+      background-color: #49443c;
+      color: #fff;
+      padding: 10px;
+      text-align: center;
+      font-weight: bold;
+      font-size: 16pt;
+    }
+
+    .frame-268 {
+      align-items: flex-end;
+      border: 1px none;
+      display: flex;
+      flex-direction: column;
+      overflow: hidden;
+      flex: 1;
+      position: relative;
+    }
+
+    .overlap-group {
+      height: 100%;
+      position: relative;
+    }
+
+    .pexels-gustavo-fring-7446987-1 {
+      position: absolute;
+      top: 0;
+      left: 0;
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+      background-image: url("images/pexels-gustavo-fring-7446987.jpg");
+      z-index: -1;
+    }
+
+    .paediatrics-logo-1 {
+      position: absolute;
+      top: 40px;
+      left: 51px;
+      width: 267px;
+      height: 113px;
+    }
+
+    .help-booking-online {
+      background-color: #545557;
+      color: #fff;
+      padding: 10px;
+      text-align: center;
+      font-weight: lighter;
+      font-size: 16px;
+      margin-bottom: 10px;
+    }
+  </style>
 </head>
-<body class="form">
-	<div class="page-content">
-		<div class="form-content">
-			<!-- Left Form Section -->
-			<div class="form-left" style="margin-right: 20px;">
-				<img src="https://cdn.pixabay.com/photo/2014/12/10/10/04/teddy-562960_1280.jpg" alt="form">
-			</div>
-			
-			<!-- Right Form Section -->
-			<div class="form-right">
-				<!-- Heading -->
-				<h2>Paediatrics</h2><br>
-				<h1>Welcome Back</h1><br>
-				
-				<!-- Body Text -->
-				<div class="body" style="text-align: center;">
-					Welcome back! Please enter your details.
-				</div>
-				
-				<!-- Sign In and Sign Up Tabs -->
-				<div class="contact">
-					<div class="tab">
-						<div class="tab-inner">
-							<button class="tablinks" onclick="openCity(event, 'sign-in')" id="defaultOpen">Sign In</button>
-						</div>
-						<div class="tab-inner">
-							<button class="tablinks" onclick="openCity(event, 'sign-up')">Sign Up</button>
-						</div>
-					</div>
-					
-					<!-- Sign In Form -->
-					<form class="form-detail" action="signin.php" method="post"> <!-- Replace "signin.php" with the PHP file to process the sign-in form -->
-						<div class="tabcontent" id="sign-in">
-							<!-- Username Field -->
-							<div class="form-row">
-								<label class="form-row-inner">
-									<input type="text" name="username" id="username" class="input-text" required>
-									<span class="label">Username</span>
-									<span class="border"></span>
-								</label>
-							</div>
-							
-							<!-- Email Field -->
-							<div class="form-row">
-								<label class="form-row-inner">
-									<input type="text" name="your_email" id="your_email" class="input-text" required>
-									<span class="label">E-Mail</span>
-									<span class="border"></span>
-								</label>
-							</div>
-							
-							<!-- Password Field -->
-							<div class="form-row">
-								<label class="form-row-inner">
-									<input type="password" name="password" id="password" class="input-text" required>
-									<span class="label">Password</span>
-									<span class="border"></span>
-								</label>
-							</div>
-							
-							<!-- Confirm Password Field -->
-							<div class="form-row">
-								<label class="form-row-inner">
-									<input type="password" name="confirm_password" id="confirm_password" class="input-text" required>
-									<span class="label">Confirm Password</span>
-									<span class="border"></span>
-								</label>
-							</div>
-							
-							<!-- Sign In Button -->
-							<div class="form-row-last">
-								<input type="submit" name="signin" class="register" value="Sign In">
-							</div>
-						</div>
-					</form>
-					
-					<!-- Sign Up Form -->
-					<form class="form-detail" action="register.php" method="post"> <!-- Replace "register.php" with the PHP file to process the registration form -->
-						<div class="tabcontent" id="sign-up">
-							<!-- Full Name Field -->
-							<div class="form-row">
-								<label class="form-row-inner">
-									<input type="text" name="full_name" id="full_name" class="input-text" required>
-									<span class="label">Username</span>
-									<span class="border"></span>
-								</label>
-							</div>
-							
-							<!-- Email Field -->
-							<div class="form-row">
-								<label class="form-row-inner">
-									<input type="text" name="your_email" id="your_email" class="input-text" required>
-									<span class="label">E-Mail</span>
-									<span class="border"></span>
-								</label>
-							</div>
-							
-							<!-- Password Field -->
-							<div class="form-row">
-								<label class="form-row-inner">
-									<input type="password" name="password" id="password" class="input-text" required>
-									<span class="label">Password</span>
-									<span class="border"></span>
-								</label>
-							</div>
-							
-							<!-- Confirm Password Field -->
-							<div class="form-row">
-								<label class="form-row-inner">
-									<input type="password" name="confirm_password" id="confirm_password" class="input-text" required>
-									<span class="label">Confirm Password</span>
-									<span class="border"></span>
-								</label>
-							</div>
-							
-							<!-- Register Button -->
-							<div class="form-row-last">
-								<input type="submit" name="register" class="register" value="Register">
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-		
-	</div>
-	<!--Footer --->
-	<footer class="footer mt-auto py-3 bg-light">
-  <div class="container">
-    <span class="text-muted">Place sticky footer content here.</span>
-  </div>
-</footer>
 
-	<!--End of footer-->
-	<script type="text/javascript">
-		function openCity(evt, cityName) {
-			var i, tabcontent, tablinks;
-			tabcontent = document.getElementsByClassName("tabcontent");
-			for (i = 0; i < tabcontent.length; i++) {
-				tabcontent[i].style.display = "none";
-			}
-			tablinks = document.getElementsByClassName("tablinks");
-			for (i = 0; i < tablinks.length; i++) {
-				tablinks[i].className = tablinks[i].className.replace(" active", "");
-			}
-			document.getElementById(cityName).style.display = "block";
-			evt.currentTarget.className += " active";
-		}
+<body>
+<div class="full-height">
+  <center>
+    <table border="0">
+      <tr>
+        <td width="80%">
+          <img src="images/paediatrics-logo-full-colour.png" alt="Paediatrics Logo" class="paediatrics-logo-1">
+        </td>
+        <td width="10%">
+          <a href="login.php" class="non-style-link"><p class="nav-item">LOGIN</p></a>
+        </td>
+        <td width="10%">
+          <a href="signup.php" class="non-style-link"><p class="nav-item" style="padding-right: 10px;">REGISTER</p></a>
+        </td>
+      </tr>
 
-		// Get the element with id="defaultOpen" and click on it
-		document.getElementById("defaultOpen").click();
-	</script>
+        <td colspan="3">
+          <p class="heading-text">Avoid Hassles & Delays.</p>
+        </td>
+      </tr>
+      <tr>
+        <td colspan="3">
+          <p class="sub-text2">How is your health today? Sounds like not good!<br>Don't worry. Find your doctor online and book an appointment with eDoc.<br>
+            We offer you a free doctor channeling service. Make your appointment now.</p>
+        </td>
+      </tr>
+      <tr>
+        <td colspan="3">
+          <div class="doctors-on-call-drop poppins-bold-cerulean-24px">
+            <select>
+              <option value="">Select a doctor</option>
+              <option value="Dr. Molefe">Dr. Molefe</option>
+              <option value="Dr. Knight">Dr. Knight</option>
+              <option value="Dr. Marya Smith">Dr. Marya Smith</option>
+              <option value="Dr. Eric Makibelo">Dr. Eric Makibelo</option>
+            </select>
+          </div>
+        </td>
+      </tr>
+      <tr>
+        <td colspan="3">
+          <center>
+            <a href="login.php">
+              <input type="button" value="Make Appointment" class="login-btn btn-primary btn" style="padding-left: 25px;padding-right: 25px;padding-top: 10px;padding-bottom: 10px;">
+            </a>
+          </center>
+        </td>
+      </tr>
+      <tr>
+        <td colspan="3">
+
+        </td>
+      </tr>
+    </table>
+    <p class="sub-text2 footer-hashen">A Web Solution by Hashen.</p>
+  </center>
+
+  <div class="weekly-calendar poppins-bold-cerulean-24px">Weekly calendar</div>
+</div>
+
+<div class="rectangle-32"></div>
+
+<div class="help-booking-online">
+  NEED HELP BOOKING ONLINE? +27 (0) 10 648 9200 admin@paediatrics.co.za
+</div>
+<div class="footer">
+  <p>2023 All rights are reserved to Paediatrics Healthcare</p>
+</div>
+
+<!--End of footer-->
+<script type="text/javascript">
+  function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
+</script>
 </body>
 </html>
---
+++ login.php
@@ -0,0 +1,172 @@
+<?php
+include "db_conn.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Receptionist Form</title>
+	<!-- Mobile -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<!-- Font-->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;700&display=swap" rel="stylesheet">
+	<!-- Main Style Css -->
+	<link rel="stylesheet" href="./css/style.css">
+</head>
+<body class="form">
+	<div class="page-content">
+		<div class="form-content">
+			<!-- Left Form Section -->
+			<div class="form-left" style="margin-right: 20px;">
+				<img src="https://cdn.pixabay.com/photo/2014/12/10/10/04/teddy-562960_1280.jpg" alt="form">
+			</div>
+			
+			<!-- Right Form Section -->
+			<div class="form-right">
+				<!-- Heading -->
+				<h2>Paediatrics</h2><br>
+				<h1>Welcome Back</h1><br>
+				
+				<!-- Body Text -->
+				<div class="body" style="text-align: center;">
+					Welcome back! Please enter your details.
+				</div>
+				
+				<!-- Sign In and Sign Up Tabs -->
+				<div class="contact">
+					<div class="tab">
+						<div class="tab-inner">
+							<button class="tablinks" onclick="openCity(event, 'sign-in')" id="defaultOpen">Sign In</button>
+						</div>
+						<div class="tab-inner">
+							<button class="tablinks" onclick="openCity(event, 'sign-up')">Sign Up</button>
+						</div>
+					</div>
+					
+					<!-- Sign In Form -->
+					<form class="form-detail" action="signin.php" method="post"> <!-- Replace "signin.php" with the PHP file to process the sign-in form -->
+						<div class="tabcontent" id="sign-in">
+							<!-- Username Field -->
+							<div class="form-row">
+								<label class="form-row-inner">
+									<input type="text" name="username" id="username" class="input-text" required>
+									<span class="label">Username</span>
+									<span class="border"></span>
+								</label>
+							</div>
+							
+							<!-- Email Field -->
+							<div class="form-row">
+								<label class="form-row-inner">
+									<input type="text" name="your_email" id="your_email" class="input-text" required>
+									<span class="label">E-Mail</span>
+									<span class="border"></span>
+								</label>
+							</div>
+							
+							<!-- Password Field -->
+							<div class="form-row">
+								<label class="form-row-inner">
+									<input type="password" name="password" id="password" class="input-text" required>
+									<span class="label">Password</span>
+									<span class="border"></span>
+								</label>
+							</div>
+							
+							<!-- Confirm Password Field -->
+							<div class="form-row">
+								<label class="form-row-inner">
+									<input type="password" name="confirm_password" id="confirm_password" class="input-text" required>
+									<span class="label">Confirm Password</span>
+									<span class="border"></span>
+								</label>
+							</div>
+							
+							<!-- Sign In Button -->
+							<div class="form-row-last">
+								<input type="submit" name="signin" class="register" value="Sign In">
+							</div>
+						</div>
+					</form>
+					
+					<!-- Sign Up Form -->
+					<form class="form-detail" action="register.php" method="post"> <!-- Replace "register.php" with the PHP file to process the registration form -->
+						<div class="tabcontent" id="sign-up">
+							<!-- Full Name Field -->
+							<div class="form-row">
+								<label class="form-row-inner">
+									<input type="text" name="full_name" id="full_name" class="input-text" required>
+									<span class="label">Username</span>
+									<span class="border"></span>
+								</label>
+							</div>
+							
+							<!-- Email Field -->
+							<div class="form-row">
+								<label class="form-row-inner">
+									<input type="text" name="your_email" id="your_email" class="input-text" required>
+									<span class="label">E-Mail</span>
+									<span class="border"></span>
+								</label>
+							</div>
+							
+							<!-- Password Field -->
+							<div class="form-row">
+								<label class="form-row-inner">
+									<input type="password" name="password" id="password" class="input-text" required>
+									<span class="label">Password</span>
+									<span class="border"></span>
+								</label>
+							</div>
+							
+							<!-- Confirm Password Field -->
+							<div class="form-row">
+								<label class="form-row-inner">
+									<input type="password" name="confirm_password" id="confirm_password" class="input-text" required>
+									<span class="label">Confirm Password</span>
+									<span class="border"></span>
+								</label>
+							</div>
+							
+							<!-- Register Button -->
+							<div class="form-row-last">
+								<input type="submit" name="register" class="register" value="Register">
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+		
+	</div>
+	<!--Footer --->
+	<footer class="footer mt-auto py-3 bg-light">
+  <div class="container">
+    <span class="text-muted">Place sticky footer content here.</span>
+  </div>
+</footer>
+
+	<!--End of footer-->
+	<script type="text/javascript">
+		function openCity(evt, cityName) {
+			var i, tabcontent, tablinks;
+			tabcontent = document.getElementsByClassName("tabcontent");
+			for (i = 0; i < tabcontent.length; i++) {
+				tabcontent[i].style.display = "none";
+			}
+			tablinks = document.getElementsByClassName("tablinks");
+			for (i = 0; i < tablinks.length; i++) {
+				tablinks[i].className = tablinks[i].className.replace(" active", "");
+			}
+			document.getElementById(cityName).style.display = "block";
+			evt.currentTarget.className += " active";
+		}
+
+		// Get the element with id="defaultOpen" and click on it
+		document.getElementById("defaultOpen").click();
+	</script>
+</body>
+</html>
